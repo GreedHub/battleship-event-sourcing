60d442b17d6466126c58cebc108039915133f84c
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-01-09 15:08:41 -0300

    updated gw api expose

--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { Application } from "../deps.ts";
 import wsServer from "./websockets/server.ts";
 import HealthRouter from "./routes/health.ts";
+const PORT = Deno.env.HTTP_PORT || 5000
 
 wsServer();
 
@@ -8,4 +9,5 @@ const app = new Application();
 
 app.use(HealthRouter.routes());
 
-await app.listen(`localhost:5000`);
+console.log("API listening on http://localhost:5000/");
+await app.listen({port: PORT});
