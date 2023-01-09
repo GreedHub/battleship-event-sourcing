75df94e9f340c43ba5480cbdb62143acad7b319a
Lucas Julian Guerci <lucas.guerci@gmail.com>
2023-01-09 20:55:06 -0300

    implemented base ws on frontend

--- backend/gateway/src/helpers/env.ts
+++ backend/gateway/src/helpers/env.ts
@@ -5,7 +5,7 @@ const configData = await config();
 export const GetWsPort = (): number => {
   const DEFAULT_PORT = 8000;
   const ENV_PORT = configData.WS_PORT;
-  let PORT = parseInt(ENV_PORT) || DEFAULT_PORT;
+  const PORT = parseInt(ENV_PORT) || DEFAULT_PORT;
   return PORT;
 };
 
--- backend/gateway/src/main.ts
+++ backend/gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { Application } from "../deps.ts";
 import wsServer from "./websockets/server.ts";
 import HealthRouter from "./routes/health.ts";
-const PORT = Deno.env.HTTP_PORT || 5000
+const PORT = Deno.env.get("HTTP_PORT") || 5000;
 
 wsServer();
 
@@ -10,4 +10,4 @@ const app = new Application();
 app.use(HealthRouter.routes());
 
 console.log("API listening on http://localhost:5000/");
-await app.listen({port: PORT});
+await app.listen({ port: PORT });
